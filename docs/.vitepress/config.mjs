51c4fa54477f514f23f5360711fbd3be6dd6fe0e
Zhengyu <2069807999@qq.com>
2024-07-18 13:31:21 +0800

    修改依赖-增加formatter

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,46 +1,94 @@
 import { defineConfig } from "vitepress";
 import { withPwa } from "@vite-pwa/vitepress";
-import { SearchPlugin } from "vitepress-plugin-search";
+// import { SearchPlugin } from "vitepress-plugin-search";
 import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
-import viteCompression from 'vite-plugin-compression';
+// import viteCompression from 'vite-plugin-compression';
+import { generateSidebar } from 'vitepress-sidebar';
 
-import { sidebarData } from "./sidebar.js";
+// import { sidebarData } from "./sidebar.js";
 
+
+//现在使用flex-search,找到了minisearch新的解决方案，效果比这个好
 // 载入模块
 // var Segment = require('segment');
-import Segment from 'segment'
+// import Segment from 'segment'
 // 创建实例
-var segment = new Segment();
+// var segment = new Segment();
 // 使用默认的识别模块及字典，载入字典文件需要1秒，仅初始化时执行一次即可
-segment.useDefault();
+// segment.useDefault();
+// var searchOptions = {
 
-var searchOptions = {
+//   //采用分词器优化，解决汉字搜索问题。效果和大小折中
+//   //来源：https://github.com/emersonbottero/vitepress-plugin-search/issues/11
+//   encode: function (str) {
+//     return segment.doSegment(str, { simple: true });
+//   },
+//   tokenize: "foward",
 
-  //采用分词器优化，解决汉字搜索问题。效果和大小折中
-  //来源：https://github.com/emersonbottero/vitepress-plugin-search/issues/11
-  encode: function (str) {
-    return segment.doSegment(str, { simple: true });
-  },
-  tokenize: "foward",
+//   // 以下代码返回完美的结果，但内存与空间消耗巨大
+//   // encode: false,
+//   // tokenize: "full",
 
-  // 以下代码返回完美的结果，但内存与空间消耗巨大
-  // encode: false,
-  // tokenize: "full",
+//   //官方推荐方法，不是很理想，留作备用
+//   // encode: false,
+//   // tokenize: function (str) {
+//   //   return segment.doSegment(str, { simple: true });
+//   // }
+//   // encode: str => str.replace(/[\x00-\x7F]/g, "").split("")
+// };
 
-  //官方推荐方法，不是很理想，留作备用
-  // encode: false,
-  // tokenize: function (str) {
-  //   return segment.doSegment(str, { simple: true });
-  // }
-  // encode: str => str.replace(/[\x00-\x7F]/g, "").split("")
-};
-
-var compressOptions = {
-  verbose: true,
-  disable: false,
-  threshold: 1024,
-  algorithm: "brotliCompress",
-  ext: ".br",
+//用于压缩，cfpages已经自动用了，如果自己部署需要打开
+// var compressOptions = {
+//   verbose: true,
+//   disable: false,
+//   threshold: 1024,
+//   algorithm: "brotliCompress",
+//   ext: ".br",
+// }
+const vitepressSidebarOptions =
+{
+  /*
+   * For detailed instructions, see the links below:
+   * https://vitepress-sidebar.jooy2.com/guide/api
+   */
+  documentRootPath: '/docs',
+  // scanStartPath: null,
+  // resolvePath: null,
+  // useTitleFromFileHeading: true,
+  useTitleFromFrontmatter: true,
+  // frontmatterTitleFieldName: 'title',
+  // useFolderTitleFromIndexFile: false,
+  useFolderLinkFromIndexFile: true,
+  // hyphenToSpace: true,
+  // underscoreToSpace: true,
+  // capitalizeFirst: false,
+  // capitalizeEachWords: false,
+  collapsed: true,
+  // collapseDepth: 2,
+  // sortMenusByName: false,
+  // sortMenusByFrontmatterOrder: false,
+  // sortMenusByFrontmatterDate: false,
+  // sortMenusOrderByDescending: false,
+  // sortMenusOrderNumericallyFromTitle: true,
+  sortMenusOrderNumericallyFromLink: true,
+  // frontmatterOrderDefaultValue: 0,
+  // manualSortFileNameByPriority: ['first.md', 'second', 'third.md'],
+  // removePrefixAfterOrdering: false,
+  // prefixSeparator: '.',
+  // excludeFiles: ['first.md', 'secret.md'],
+  excludeFilesByFrontmatterFieldName: 'exclude',
+  // excludeFolders: ['secret-folder'],
+  // includeDotFiles: false,
+  // includeRootIndexFile: false,
+  // includeFolderIndexFile: false,
+  // includeEmptyFolder: false,
+  // rootGroupText: 'Contents',
+  // rootGroupLink: 'https://github.com/jooy2',
+  // rootGroupCollapsed: false,
+  // convertSameNameSubFileToGroupIndexPage: false,
+  // folderLinkNotIncludesFileName: false,
+  // keepMarkdownSyntaxFromTitle: false,
+  // debugPrint: false,
 }
 
 // https://vitepress.dev/reference/site-config
@@ -62,14 +110,47 @@ export default withPwa(
       outline: {
         level: [1, 3],
       },
-      // search: {
-      //   provider: "local",
-      // },
+      search: {
+        provider: "local",
+        options: {
+          miniSearch: {
+            options: {
+              tokenize: (term) => {
+                if (typeof term === 'string') term = term.toLowerCase();
+                // @ts-ignore
+                const segmenter = Intl.Segmenter && new Intl.Segmenter("zh", { granularity: "word" });
+                if (!segmenter) return [term];
+                const tokens = [];
+                for (const seg of segmenter.segment(term)) {
+                  // @ts-ignore
+                  tokens.push(seg.segment);
+                }
+                return tokens;
+              },
+            },
+            searchOptions: {
+              combineWith: 'AND', // important for search chinese
+              processTerm: (term) => {
+                if (typeof term === 'string') term = term.toLowerCase();
+                // @ts-ignore
+                const segmenter = Intl.Segmenter && new Intl.Segmenter("zh", { granularity: "word" });
+                if (!segmenter) return term;
+                const tokens = [];
+                for (const seg of segmenter.segment(term)) {
+                  // @ts-ignore
+                  tokens.push(seg.segment);
+                }
+                return tokens;
+              },
+            },
+          },
+        },
+      },
       editLink: {
         pattern: "https://github.com/ZYL9/PFRPG/edit/main/docs/:path",
         text: "Edit this page on GitHub",
       },
-      sidebar: sidebarData,
+      sidebar: generateSidebar(vitepressSidebarOptions)
     },
     markdown: {
       image: {
@@ -82,7 +163,7 @@ export default withPwa(
     lang: "zh-cn",
     vite: {
       plugins: [
-        SearchPlugin(searchOptions),
+        // SearchPlugin(searchOptions),//使用flex-search,找到了minisearch新的解决方案，效果比这个好
         chunkSplitPlugin(),
         // viteCompression(compressOptions)//用于压缩，cfpages已经自动用了，如果自己部署需要打开
 
